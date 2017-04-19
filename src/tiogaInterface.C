9bf98ef5e4f2081ae7adfeb3fbe636a324ebec3e
JacobCrabill <jcrabill@stanford.edu>
2017-04-19 15:19:20 -0700

    Bug fix function dataUpdate calls from tiogaInterface.

--- src/tiogaInterface.C
+++ src/tiogaInterface.C
@@ -193,12 +193,20 @@ extern "C" {
 
   void tioga_dataupdate_ab_send(int nvar, int gradFlag)
   {
+#ifdef _GPU
+    tg->dataUpdate_artBnd_send(nvar, gradFlag);
+#else
     tg->dataUpdate_artBnd_send(nvar, NULL, gradFlag);
+#endif
   }
 
   void tioga_dataupdate_ab_recv(int nvar, int gradFlag)
   {
+#ifdef _GPU
+    tg->dataUpdate_artBnd_recv(nvar, gradFlag);
+#else
     tg->dataUpdate_artBnd_recv(nvar, NULL, gradFlag);
+#endif
   }
 
   void tioga_writeoutputfiles_(double *q,int *nvar,char *itype)

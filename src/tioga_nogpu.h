a89053efc3c67b1181170ca2933f22052ed5d343
Shreyas Ananthan <shreyas@umd.edu>
2020-09-04 10:32:55 -0600

    GPU specific headers

---
+++ src/tioga_nogpu.h
@@ -0,0 +1,57 @@
+#ifndef TIOGA_NOGPU_H
+#define TIOGA_NOGPU_H
+
+#include <cstdlib>
+
+#define TIOGA_GPU_DEVICE
+#define TIOGA_GPU_GLOBAL
+#define TIOGA_GPU_HOST
+#define TIOGA_GPU_HOST_DEVICE
+
+namespace TIOGA {
+namespace gpu {
+
+using gpuError_t = int;
+constexpr gpuError_t gpuSuccess = 0;
+
+inline gpuError_t gpuGetLastError() { return gpuSuccess; }
+inline const char* gpuGetErrorString(gpuError_t err) { return "Success"; }
+
+template <typename T>
+inline T* allocate_on_device(const size_t size)
+{
+    return static_cast<T*>(std::malloc(size));
+}
+
+template <typename T>
+inline void copy_to_device(T* dptr, const T* hptr, const size_t size)
+{
+    std::memcpy(dptr, hptr, size);
+}
+
+template <typename T>
+inline T* push_to_device(const T* hptr, const size_t size)
+{
+    T* dptr = allocate_on_device<T>(size);
+    std::memcpy(dptr, hptr, size);
+    return dptr;
+}
+
+template <typename T>
+inline void pull_from_device(T* hptr, T* dptr, const size_t size)
+{
+    std::memcpy(hptr, dptr, size);
+}
+
+template <typename T>
+inline void deallocate_device(T** dptr)
+{
+    std::free(*dptr);
+    *dptr = nullptr;
+}
+
+}
+}
+
+
+#endif /* TIOGA_NOGPU_H */

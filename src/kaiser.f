e1236d74b38b3f401c27813c3b6c86f37c01315b
Ilker Topcuoglu <114435459+itopcuoglu@users.noreply.github.com>
2025-03-07 11:56:42 -0700

    Changing the license from LGPL 2.1 to BSD 3-Clause (#71)
    * BSD 3-Clause License files
    
    * Modifying the license headers for source files
    
    * Updating COPYRIGHT
    
    * Update README
    
    ---------
    
    Co-authored-by: jsitaraman <jsitaraman@gmail.com>

--- src/kaiser.f
+++ src/kaiser.f
@@ -1,22 +1,9 @@
 c$$$
-c$$$This file is part of the Tioga software library
+c$$$ Copyright TIOGA Developers. See COPYRIGHT file for details.
 c$$$
-c$$$Tioga  is a tool for overset grid assembly on parallel distributed systems
-c$$$Copyright (C) 2015 Jay Sitaraman
-c$$$
-c$$$This library is free software; you can redistribute it and/or
-c$$$modify it under the terms of the GNU Lesser General Public
-c$$$License as published by the Free Software Foundation; either
-c$$$version 2.1 of the License, or (at your option) any later version.
-c$$$
-c$$$This library is distributed in the hope that it will be useful,
-c$$$but WITHOUT ANY WARRANTY; without even the implied warranty of
-c$$$MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-c$$$Lesser General Public License for more details.
-c$$$
-c$$$You should have received a copy of the GNU Lesser General Public
-c$$$License along with this library; if not, write to the Free Software
-c$$$Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+c$$$ SPDX-License-Identifier: (BSD 3-Clause)
+
+
       subroutine kaiser_wrap(a,nrows,n,eigenv,trace,sume,ier)
       integer, intent(inout) :: nrows,n
       real*8, intent(inout):: a(nrows*n)
